4552eaa1ba858376e9435cfb94d7786f5b390f96
Mavrick Martinek <mavrick.martinek@dcmail.ca>
2018-11-12 13:33:15 -0500

    new migrations

---
+++ database/migrations.1/2018_11_09_163736_create_payments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->increments('payment_id');
+            $table->string('type');
+            $table->integer('total_fee');
+            $table->integer('amount_paid');
+            $table->time('timestamp');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
