5b3aed254be1191eb5d9acb193492f4807287e97
KeenanBC <ke.j.binniecormier@gmail.com>
2018-11-26 16:19:22 -0500

    some change

--- app/Payment.php
+++ app/Payment.php
@@ -6,20 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    public function publish()
-    {
-        
-             Event::create([
-             'type' => request('type'),
-             'number'=> request('number'),
-             'expiry'=> request('expiry'),
-             'name'=> request('name'),
-             'charge'=> request('charge')
-
-          
-             
-             //'user_id' => auth()->id()
-         ]);
-         
-    }
+  
 }
--- database/migrations/2018_11_21_163115_create_payments_table.php
+++ database/migrations/2018_11_21_163115_create_payments_table.php
@@ -15,7 +15,7 @@ class CreatePaymentsTable extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->increments('id');
-            $table->user_id('user_id');
+            //$table->user_id('user_id');
             $table->string('type');
             $table->bigInteger('number');
             $table->date('expiry');
